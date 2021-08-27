2f8e9d10bb6f64fa7792cf5fb103964f312a6dc3
ajscholz <yoitsscholzy@gmail.com>
2021-08-27 14:01:55 -0400

    add contentful banner data to index page

--- src/components/layout/IndexHero.jsx
+++ src/components/layout/IndexHero.jsx
@@ -1,24 +1,27 @@
 /* This example requires Tailwind CSS v2.0+ */
 import React from 'react'
+import PropTypes from 'prop-types'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
-export default function Example() {
+const IndexHero = ({ banner }) => {
+  const { heroText, image } = banner
+  const text = heroText.split(' ')
+
   return (
     <div className='relative bg-gray-50'>
       <main className='lg:relative'>
         <div className='mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left'>
           <div className='px-4 lg:w-1/2 sm:px-8 xl:pr-16'>
             <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl'>
-              <span className='block xl:inline'>Data to enrich your</span>{' '}
-              <span className='block text-blue-600 xl:inline'>
-                online business
-              </span>
+              <span className='block'>{text[0]}</span>
+              <span className='block text-blue-600 xl:inline'>{text[1]}</span>
             </h1>
-            <p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
+            {/* <p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
               lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
               fugiat aliqua.
-            </p>
-            <div className='mt-10 sm:flex sm:justify-center lg:justify-start'>
+            </p> */}
+            {/* <div className='mt-10 sm:flex sm:justify-center lg:justify-start'>
               <div className='rounded-md shadow'>
                 <a
                   href='#'
@@ -35,13 +38,13 @@ export default function Example() {
                   Live demo
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className='relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full'>
-          <img
+          <GatsbyImage
             className='absolute inset-0 w-full h-full object-cover'
-            src='https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80'
+            image={image.gatsbyImageData}
             alt=''
           />
         </div>
@@ -49,3 +52,12 @@ export default function Example() {
     </div>
   )
 }
+
+IndexHero.propTypes = {
+  banner: PropTypes.shape({
+    image: PropTypes.object.isRequired,
+    heroText: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default IndexHero
