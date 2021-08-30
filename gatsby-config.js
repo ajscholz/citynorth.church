74ee450847549776d8cd6a58be8519db9555c510
ajscholz <yoitsscholzy@gmail.com>
2021-08-30 18:56:53 -0400

    add contentful env variable support for preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,26 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+}
+
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
+
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
