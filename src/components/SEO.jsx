6e5235127bb26ba7582ccbf87bd14b288636276d
ajscholz <yoitsscholzy@gmail.com>
2021-09-02 14:19:34 -0400

    add seo component, add design page

---
+++ src/components/SEO.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const SEO = ({ title, description, image, slug, index }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site: contentfulChurchInformation {
+        name
+        siteUrl
+        description: seoDefaultDescription {
+          text: seoDefaultDescription
+        }
+        icon {
+          file {
+            url
+          }
+        }
+      }
+    }
+  `)
+
+  const { site } = data
+
+  return (
+    <Helmet htmlAttributes={{ lang: `en` }} titleTemplate={`%s • ${site.name}`}>
+      <title>{title}</title>
+      <meta name='description' content={description || site.description.text} />
+      {!index && <meta name='robots' content='none' />}
+      <link rel='canonical' href={`${site.siteUrl}${slug}`} />
+      <link rel='shortcut icon' href={`https:${site.icon.file.url}`} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='og:title' content={title} />
+      <meta
+        name='og:description'
+        content={description || site.description.text}
+      />
+      <meta name='og:image' content={`${image || ''}`} />
+      <meta name='og:type' content='website' />
+      <meta name='og:url' content={`${site.siteUrl}/${slug}`} />
+      <meta name='og:site_name' content={site.title} />
+    </Helmet>
+  )
+}
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  slug: PropTypes.string,
+  index: PropTypes.bool.isRequired,
+}
+
+SEO.defaultProps = {
+  index: true,
+}
+
+export default SEO
---
+++ src/pages/design.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SEO from '../components/SEO'
+
+const DesignPage = () => {
+  return (
+    <>
+      <SEO index={false} />
+      <div>Design Page</div>
+    </>
+  )
+}
+
+export default DesignPage
